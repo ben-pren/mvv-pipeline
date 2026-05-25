7dd85cda41d553cf610204a5f3cd5f20199b90bc
Benjamin Prandtl <bennj-p@web.de>
2026-05-25 17:07:42 +0200

    transform verbessert

--- transformation/transform.py
+++ transformation/transform.py
@@ -1,4 +1,20 @@
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
+from datetime import datetime
+
+
+def parse_time (time: str | None) -> str | None:
+    if time is None:
+        return None 
+    dt = datetime.strptime(time, "%d%m%y%H%M")
+    return dt.strftime("%Y-%m-%d %H:%M")
+
+def calculate_delay (pt: str | None, ct: str | None) -> int | None:
+    if pt is None or ct is None:
+        return None
+    pt = datetime.strptime(pt, "%d%m%y%H%M")
+    ct = datetime.strptime(ct, "%d%m%y%H%M")
+    verspaetung = (ct - pt).seconds // 60
+    return verspaetung 
 
 def transform_raw_data ( data: str,) -> list[dict]:
     root = ET.fromstring(data)
@@ -6,11 +22,13 @@ def transform_raw_data ( data: str,) -> list[dict]:
     for s in root:
         if s.find("dp") is not None:
             dp = s.find("dp")
-            new_list.append({"zug": dp.get("fb"),
-                             "geplante_zeit": dp.get("pt"),
-                             "tatsaechliche_zeit": dp.get("ct"),
+            if dp.get("fb") is not None or dp.get("l") is not None:
+                new_list.append({"zug": dp.get("fb"),
+                             "geplante_zeit": parse_time(dp.get("pt")),
+                             "tatsaechliche_zeit": parse_time(dp.get("ct")),
                              "gleis": dp.get("pp"),
                              "linie": dp.get("l"),
-                             "richtung": dp.get("ppth")
+                             "richtung": dp.get("ppth"),
+                             "verspaetung_min" : calculate_delay(dp.get("pt"), dp.get("ct"))
                              })
     return new_list   
